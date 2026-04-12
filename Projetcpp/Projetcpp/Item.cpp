12050a641a57848952b327ce4d105faaa81aec23
maximelrnt <maxime.laurent@edu.devinci.fr>
2026-04-12 12:29:17 +0200

    suppression des doublons

--- Projetcpp/Projetcpp/Item.cpp
+++
@@ -1,9 +0,0 @@
-#include "Item.h"
-#include <iostream>
-
-using namespace std;
-
-void Item::display() const {
-    cout << "  - " << this->m_name << " [" << this->m_type << "] +" << this->m_value
-        << " HP | Quantite : " << this->m_quantity << endl;
-}
